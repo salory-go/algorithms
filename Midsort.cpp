b358b68ca01c8471b391290eb0fdcd1b7b2948d5
EnCoUnTeR <102663083+salory-go@users.noreply.github.com>
2022-12-26 14:36:14 +0800

    归并排序

--- Midsort.cpp
+++ Midsort.cpp
@@ -1,34 +1,39 @@
 #include<cstdio>
 #include<iostream>
+
 using namespace std;
 
-const int N = 1e6 + 10;
-int tmp[N];
-void Midsort(int q[],int l,int r){
-    if (l >= r) return;
+const int N = 1e6+10;
+int q[N],a[N];
+
+
+void merge_sort(int q[],int l,int r)
+{
+    if(l>=r)return;
+    
     int mid = (l+r)/2;
-    Midsort(q, l, mid);
-    Midsort(q, mid + 1, r);
-    int k = 0, i = l, j = mid + 1;
-    while(i<=mid&&i>r){
-        if(q[i]<q[j]){tmp[k++]=q[i++];}
-        else{tmp[k++]=q[j++];}
+    merge_sort(q,l,mid),merge_sort(q,mid+1,r);
+    
+    int i=l,j=mid+1,k=0;
+    while(i<=mid&&j<=r)
+    {
+        if(q[i]<=q[j])a[k++] = q[i++];
+        else a[k++] = q[j++];
     }
-    while (i <= mid) tmp[k ++ ] = q[i ++ ];
-    while (j <= r) tmp[k ++ ] = q[j ++ ];
-    for (i = l, j = 0; i <= r; i ++, j ++ ) q[i] = tmp[j];
+    while(i<=mid)a[k++] = q[i++];
+    while(j<=r)a[k++] = q[j++];
+    
+    for(int i = l,j=0;i<=r;i++,j++)q[i] = a[j];
 }
 
 int main(){
-    int n=0;
+    int n;
     cin>>n;
-    int q[N],i=0;
-    while(i<n){
-       cin>>q[i++];
-    }
-    Midsort(q,n-1,0);
-    i=0;
-    while(i<n){
-       cout<<q[i++]<<" ";
-    }
+    for(int i=0;i<n;i++)cin>>q[i];
+    
+    merge_sort(q,0,n-1);
+    
+    for(int i=0;i<n;i++)cout<<q[i]<<" ";
+    
+    return 0;
 }
