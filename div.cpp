62aeebec539966f0dc7d49627d695737c9bbc3f0
EnCoUnTeR <102663083+salory-go@users.noreply.github.com>
2022-12-29 15:30:36 +0800

    Add files via upload

--- div.cpp
+++ div.cpp
@@ -4,16 +4,19 @@
 #include<iostream>
 
 using namespace std;
-
+// 只需要这里加&就行
 vector<int> div(vector<int> &A,int b,int &t){
     
     vector<int> C;
+    // 除法要从前面开始
     for(int i=A.size()-1;i>=0;i--)
     {
+        // 记得乘10
         t = A[i]+t*10;
         C.push_back(t/b);
         t %=b;
     }
+    // 前面的0没用，后面的0有用，所以需要反转然后消0
     reverse(C.begin(),C.end());
     while (C.size() > 1 && C.back() == 0) C.pop_back();
     return C;
