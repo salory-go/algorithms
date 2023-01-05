12cf8b11f5d96f6b5509d7aca2156d512dc34056
EnCoUnTeR <102663083+salory-go@users.noreply.github.com>
2023-01-05 20:15:21 +0800

    Add files via upload

--- Midsort.cpp
+++ Midsort.cpp
@@ -1,20 +1,14 @@
-#include<cstdio>
 #include<iostream>
-
+#include<cstdio>
 using namespace std;
-
 const int N = 1e6+10;
 int q[N],a[N];
-
-
-void merge_sort(int q[],int l,int r)
+void Binsort(int q[],int l,int r)
 {
-    if(l>=r)return;
-    
+    if(l>=r) return;
     int mid = (l+r)/2;
-    merge_sort(q,l,mid),merge_sort(q,mid+1,r);
-    
-    int i=l,j=mid+1,k=0;
+    Binsort(q,l,mid),Binsort(q,mid+1,r);
+    int i = l,j = mid+1,k = 0;
     while(i<=mid&&j<=r)
     {
         if(q[i]<=q[j])a[k++] = q[i++];
@@ -22,19 +16,15 @@ void merge_sort(int q[],int l,int r)
     }
     while(i<=mid)a[k++] = q[i++];
     while(j<=r)a[k++] = q[j++];
-    
-    for(int i = l,j=0;i<=r;i++,j++)q[i] = a[j];
+    for(int i=l,j=0;i<=r;i++,j++)q[i] = a[j];
 }
 
 int main(){
     int n;
     cin>>n;
-    for(int i=0;i<n;i++)cin>>q[i];
-    
-    merge_sort(q,0,n-1);
-    
-    for(int i=0;i<n;i++)cout<<q[i]<<" ";
-    
-    return 0;
+    for(int i=0;i<n;i++)scanf("%d",&q[i]);
+    Binsort(q,0,n-1);
+    for(int i=0;i<n;i++)printf("%d ",q[i]);
 }
 //无处理的二分递归，然后注意<=，注意合并数组
+//指针不要搞错了，归并的逻辑不要搞错了
