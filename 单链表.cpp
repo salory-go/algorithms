d4080ce0c1981b911de64866aea79d6331b67c10
EnCoUnTeR <102663083+salory-go@users.noreply.github.com>
2023-01-06 18:09:20 +0800

    Add files via upload

--- 单链表.cpp
+++ 单链表.cpp
@@ -1,33 +1,29 @@
 #include<iostream>
-#include<cstdio>
 using namespace std;
-
-const int M = 1e5+10;
-
-
-int e[M],ne[M],head,indx;
+const int N = 1e5+10;
+int e[N],ne[N],head,idx;
 
 void init()
 {
-    head = -1;
-    indx = 0;
+    head=-1;
+    idx=0;
 }
 
-void add_to_head(int x)
+void insert_head(int x)
 {
-    e[indx] = x;
-    ne[indx] = head;
-    head = indx++;
+    e[idx] = x;
+    ne[idx] = head;
+    head = idx++;
 }
 
 void insert(int k,int x)
 {
-    e[indx] = x;
-    ne[indx] = ne[k];
-    ne[k] = indx++;
+    e[idx] = x;
+    ne[idx] = ne[k];
+    ne[k] = idx++;
 }
 
-void remove(int k)
+void del(int k)
 {
     ne[k] = ne[ne[k]];
 }
@@ -36,29 +32,30 @@ int main(){
     int n;
     cin>>n;
     init();
-    while(n--){
-        char c;
-        cin>>c;
-        if(c=='H')
+    while(n--)
+    {
+        char op[2];
+        scanf("%s",&op);
+        if(op[0]=='H')
         {
             int x;
             cin>>x;
-            add_to_head(x);
+            insert_head(x);
         }
-        else if(c=='I')
+        else if(op[0]=='I')
         {
             int x,k;
             cin>>k>>x;
             insert(k-1,x);
         }
-        else if(c=='D')
-        {
+        else if(op[0]=='D'){
             int k;
             cin>>k;
             if(k==0)head = ne[head];
-            remove(k-1);
+            del(k-1);
         }
     }
-    for(int i=head;i!=-1;i = ne[i])cout<<e[i]<<" ";
-    
+    for(int i=head;i!=-1;i=ne[i])cout<<e[i]<<" ";
 }
+// idx下标就是第几个插入的数，ne的下标和值都是idx，初始化有head，k要减一，因为idx从1开始
+// remove都是一样的，head是-1，最后也是-1
