a9f9982ef8ee56b55f8ba0dfd6e9ce5ab9a3129c
EnCoUnTeR <102663083+salory-go@users.noreply.github.com>
2023-01-06 10:34:45 +0800

    Add files via upload

--- 最长不重复序列.cpp
+++ 最长不重复序列.cpp
@@ -1,28 +1,18 @@
 #include<iostream>
-#include<string.h>
-
 using namespace std;
-const int N = 100010;
-int a[N],s[N];
+const int N = 1e5+10;
+int q[N],s[N];
 
 int main(){
-    int n;
+    int n,res=0;
     cin>>n;
-    for(int i=0;i<n;i++)cin>>a[i];
-    
-    int res=0;
+    for(int i=0;i<n;i++)cin>>q[i];
     for(int i=0,j=0;i<n;i++)
     {
-        s[a[i]]++;   
-        while(s[a[i]]>1)
-        {
-            s[a[j]]--;
-            j++;
-        }
-        res = max(i-j+1,res);
-        
+        s[q[i]]++;
+        while(s[q[i]]>1)s[q[j++]]--;
+        res = max(res,i-j+1);
     }
     cout<<res;
-    
-    return 0;
 }
+//双指针是指外for内while，check序列是否重复用的是新数组存数字出现次数
