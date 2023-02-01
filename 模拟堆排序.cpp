28d4e74c0ef343b31754f83bf92da364d63caef0
EnCoUnTeR <102663083+salory-go@users.noreply.github.com>
2023-02-01 19:33:46 +0800

    Add files via upload

--- 模拟堆排序.cpp
+++ 模拟堆排序.cpp
@@ -1,76 +1,76 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <cstdio>
+
 using namespace std;
 
-const int N = 1e5+10;
-int h[N],sz=0,hp[N],ph[N];
+const int N = 100010;
 
-void heap_swap(int a,int b)
+int h[N], hp[N], ph[N];
+int n, m, s;
+
+void heap_swap(int a, int b)
 {
-    swap(h[a],h[b]);
-    
-    swap(hp[a],hp[b]);
-    
-    swap(ph[hp[a]],ph[hp[b]]);
+    swap(h[a], h[b]);
+    swap(ph[hp[a]], ph[hp[b]]);
+    swap(hp[a], hp[b]);
 }
+
 void down(int u)
 {
-    int t =u;
-    if(2*u<=sz&&h[2*u]<h[t])t = 2*u;
-    if(2*u+1<=sz&&h[2*u+1]<h[t])t = 2*u+1;
-    if(u!=t)
-    {
-        heap_swap(h[t],h[u]);
-        down(t);
-    }
+    int t = u;
+    if (u * 2 <= s && h[t] > h[2 * u]) t = 2 * u;
+    if (u * 2 + 1 <= s && h[t] > h[2 * u + 1]) t = 2 * u + 1;
+    if (t != u) heap_swap(t, u), down(t); 
 }
 
 void up(int u)
 {
-    if(u/2>0&&h[u]<h[u/2]) 
+    while (u / 2 && h[u] < h[u / 2])
     {
-        heap_swap(u,u/2);
-        up(u>>1);
+        heap_swap(u, u / 2);
+        u /= 2;
     }
 }
 
-int main(){
-    int n;
-    cin>>n;
-    int m=0;
-    while(n--)
+int main()
+{
+    scanf("%d", &n);
+
+    while (n -- )
     {
         string op;
-        int k,x; 
-        cin>>op;
-        if(op=="I")
+        int k, x;
+        cin >> op;
+
+        if (op == "I")
         {
-            scanf("%d",&x);
-            m++;
-            h[++sz] = x;
-            hp[sz]=m;
-            ph[m]=sz;
-            up(sz);
+            scanf("%d", &x);
+            s ++, m ++ ;
+            hp[s] = m, ph[m] = s;
+            h[s] = x;
+            up(s);
         }
-        else if(op=="PM")
+        else if (op == "PM") printf("%d\n", h[1]);
+        else if (op == "DM")
         {
-            printf("%d\n",h[1]);
-        }
-        else if(op=="DM")
-        {
-            heap_swap(1,sz--);  
+            heap_swap(1, s);
+            s -- ;
             down(1);
         }
-        else if(op=="D")
+        else if (op == "D")
         {
-            scanf("%d",&k);
-            heap_swap(ph[k],sz--);  
-            down(ph[k]);up(ph[k]);
+            scanf("%d", &k);
+            k = ph[k];
+            heap_swap(k, s);
+            s -- ;
+            down(k), up(k);
         }
-        else {
-            cin>>k>>x;
-            h[ph[k]] = x;
-            down(ph[k]);up(ph[k]);
+        else if (op == "C")
+        {
+            scanf("%d %d", &k, &x);
+            k = ph[k];
+            h[k] = x;
+            down(k), up(k);
         }
     }
 }
