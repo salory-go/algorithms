07ccd3f8938b873bd6ff506c42b1774dd0c43692
EnCoUnTeR <102663083+salory-go@users.noreply.github.com>
2023-02-23 19:44:27 +0800

    Create 区间选点.cpp

---
+++ 区间选点.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 1e5+10;
+
+struct Range
+{
+    int l,r;
+    bool operator< (const Range &W)const
+    {
+        return  r < W.r;
+    }
+}range[N];
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        range[i] = {a,b};
+    }
+    
+    sort(range,range+n);
+    int res = 0 ,ed =-2e9;
+    for(int i=0;i<=n;i++)
+    if(range[i].l>ed)
+    {
+        res++;
+        ed = range[i].r;
+    }
+        
+    cout<<res;
+    
+}
